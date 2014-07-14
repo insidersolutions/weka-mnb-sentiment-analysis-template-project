e4744daeb1437e8ad04625ae663e97a9374b7faf
DmitryKey <dmitry.kan@gmail.com>
2014-07-14 22:53:04 +0300

    Added performance test case and data

--- src/test/java/info/semanticanalyzer/classifiers/weka/ThreeWayMNBTrainerTest.java
+++ src/test/java/info/semanticanalyzer/classifiers/weka/ThreeWayMNBTrainerTest.java
@@ -1,6 +1,10 @@
 package info.semanticanalyzer.classifiers.weka;
 
 import junit.framework.Assert;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.FileInputStream;
 
 /**
  * Created by Dmitry Kan on 27.04.2014.
@@ -8,6 +12,7 @@ import junit.framework.Assert;
 public class ThreeWayMNBTrainerTest {
     ThreeWayMNBTrainer threeWayMnbTrainer;
     String modelFile = "models/three-way-sentiment-mnb.model";
+    private static final String PERFOMRANCE_TEST_CONTENT_FILE = "src/test/resources/en_imdb_sentences.txt";
 
     @org.junit.Before
     public void setUp() throws Exception {
@@ -66,4 +71,47 @@ public class ThreeWayMNBTrainerTest {
         threeWayMnbTrainer.loadModel(modelFile);
         Assert.assertEquals(SentimentClass.ThreeWayClazz.NEUTRAL, threeWayMnbTrainer.classify("I really don't know whether I like or dislike this weather"));
     }
+
+    @Test
+    public void testPerformance() throws Exception
+    {
+        String content = IOUtils.toString(new FileInputStream(PERFOMRANCE_TEST_CONTENT_FILE), "UTF-8");
+        String[] lines = content.split("\n");
+
+        int wordsCount = getWordsCount(lines);
+
+        threeWayMnbTrainer.loadModel(modelFile);
+
+        test(lines, wordsCount, content.length()); // warm up
+
+        test(lines, wordsCount, content.length()); // test
+    }
+
+    private int getWordsCount(String[] texts)
+    {
+        int count = 0;
+        for (String str : texts) {
+            count += str.split("\\s+").length;
+        }
+        return count;
+    }
+
+    private void test(String[] texts, int wordsCount, int totalLength) throws Exception {
+        System.out.println("Testing on " + texts.length + " samples, " + wordsCount + " words, " + totalLength
+                + " characters...");
+
+        long startTime = System.currentTimeMillis();
+        for (String str : texts) {
+            // to print out the predicted labels, uncomment the line:
+            //System.out.println(threeWayMnbTrainer.classify(str).name());
+            threeWayMnbTrainer.classify(str).name();
+        }
+        long elapsedTime = System.currentTimeMillis() - startTime;
+
+        System.out.println("Time " + elapsedTime + " ms.");
+        System.out.println("Speed " + ((double) totalLength / elapsedTime) + " chars/ms");
+        System.out.println("Speed " + ((double) wordsCount / elapsedTime) + " words/ms");
+        System.out.println("+++++++++=");
+    }
+
 }
